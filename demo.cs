ca58655ca92888cf75fbca81178347fd222dd453
Rithika-123 <115354330+Rithika-123@users.noreply.github.com>
2022-11-17 05:30:33 +0000

    Add files via upload

---
+++ demo.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace Ado.net_demo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SqlConnection conn = new SqlConnection(@"Data Source=rithikaVM;Initial Catalog=Employee;Integrated Security=True");
+
+
+
+            SqlCommand cmd = new SqlCommand("Select * from Employee", conn);
+            conn.Open();
+            SqlDataReader dr = cmd.ExecuteReader();
+
+
+
+            while (dr.Read())
+            {
+                string EmpID = dr["EmpID"].ToString();
+                string FirstName = dr["FirstName"].ToString();
+                string LastName = dr["LastName"].ToString();
+                string Salary = dr["Salary"].ToString();
+                Console.WriteLine(EmpID + " " + FirstName + " " + LastName + " " + Salary);
+            }
+
+
+
+            dr.Close();
+
+
+
+            conn.Close();
+            Console.ReadLine();
+        }
+    }
+}
